1b3359dfae6e11df4f3c0d4eade5c5e8bc677ff7
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-06-20 14:45:04 +0100

    adding methods to 'clean' the phenopackets

---
+++ src/pheval_ontogpt/prepare/clean_phenopacket.py
@@ -0,0 +1,24 @@
+from phenopackets import Phenopacket
+
+
+class PhenopacketCleaner:
+    def __init__(self, phenopacket: Phenopacket):
+        self.phenopacket = phenopacket
+
+    def remove_interpretations(self) -> None:
+        """Remove the interpretations object from a phenopacket."""
+        del self.phenopacket.interpretations[:]
+
+    def remove_diseases(self) -> None:
+        """Remove the diseases object from a phenopacket."""
+        del self.phenopacket.diseases[:]
+
+    def rename_id(self) -> None:
+        """Rename the ID for the phenopacket."""
+        self.phenopacket.id = "patient1"
+
+    def clean_phenopacket(self) -> Phenopacket:
+        self.remove_interpretations()
+        self.remove_diseases()
+        self.rename_id()
+        return self.phenopacket
