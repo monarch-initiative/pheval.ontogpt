6a3005518db7dff883ff2b2e3f94a6bcd24e66ea
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-06-20 14:44:09 +0100

    add run methods for running the basic pheno engine

---
+++ src/pheval_ontogpt/run/run_basic_pheno_engine.py
@@ -0,0 +1,31 @@
+import json
+from pathlib import Path
+
+from phenopackets import Phenopacket
+from pheval.utils.file_utils import all_files
+from pheval.utils.phenopacket_utils import phenopacket_reader
+
+from pheval_ontogpt.prepare.clean_phenopacket import PhenopacketCleaner
+from pheval_ontogpt.run.basic_pheno_engine import PhenoEngine
+
+
+def run_phenopacket(pheno_engine: PhenoEngine, phenopacket: Phenopacket):
+    return pheno_engine.predict_disease(phenopacket)
+
+
+def write_json_result(
+    ontogpt_result: [dict], raw_results_dir: Path, phenopacket_path: Path
+) -> None:
+    output_file = raw_results_dir.joinpath(f"{phenopacket_path.stem}-ontogpt_result.json")
+    with open(output_file, "w") as outfile:
+        json.dump(ontogpt_result, outfile, indent=4)
+    outfile.close()
+
+
+def run_phenopackets(phenopacket_dir: Path, raw_results_dir: Path) -> None:
+    pheno_engine = PhenoEngine()
+    for phenopacket_path in all_files(phenopacket_dir):
+        phenopacket = phenopacket_reader(phenopacket_path)
+        clean_phenopacket = PhenopacketCleaner(phenopacket).clean_phenopacket()
+        result = run_phenopacket(pheno_engine, clean_phenopacket)
+        write_json_result(result, raw_results_dir, phenopacket_path)
