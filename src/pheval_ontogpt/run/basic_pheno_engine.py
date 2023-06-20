4c2e03134a7328ec35e5913287dd3b33a807468f
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-06-20 14:45:16 +0100

    tox lint

--- src/pheval_ontogpt/run/basic_pheno_engine.py
+++ src/pheval_ontogpt/run/basic_pheno_engine.py
@@ -9,10 +9,8 @@ from jinja2 import Template
 from oaklib import get_adapter
 from oaklib.datamodels.text_annotator import TextAnnotationConfiguration
 from oaklib.interfaces import MappingProviderInterface, TextAnnotatorInterface
-from pydantic import BaseModel
-
 from ontogpt.engines.knowledge_engine import KnowledgeEngine
-from ontogpt.io.yaml_wrapper import dump_minimal_yaml
+from pydantic import BaseModel
 
 from pheval_ontogpt.promp_templates import DEFAULT_PHENOPACKET_PROMPT
 
