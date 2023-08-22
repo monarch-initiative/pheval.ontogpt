6374d14c580ab7ab0c1431487dd18303ffc8fb72
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-08-22 12:07:13 +0100

    removing objects from a phenopacket

--- src/pheval_ontogpt/prepare/clean_phenopacket.py
+++ src/pheval_ontogpt/prepare/clean_phenopacket.py
@@ -1,4 +1,6 @@
 from phenopackets import Phenopacket
+from pheval.utils.phenopacket_utils import PhenopacketUtil
+import random
 
 
 class PhenopacketCleaner:
@@ -17,9 +19,29 @@ class PhenopacketCleaner:
         """Rename the ID for the phenopacket."""
         self.phenopacket.id = "patient1"
 
+    def remove_evidence_from_phenotypic_features(self):
+        for phenotypic_feature in PhenopacketUtil(self.phenopacket).phenotypic_features():
+            del phenotypic_feature.evidence[:]
+
+    def check_length_phenotypic_features(self):
+        if len(PhenopacketUtil(self.phenopacket).phenotypic_features()) > 50:
+            selected = random.sample(PhenopacketUtil(self.phenopacket).phenotypic_features(), 50)
+            del self.phenopacket.phenotypic_features[:]
+            self.phenopacket.phenotypic_features.extend(selected)
+
+    def remove_metadata_resources(self):
+        del self.phenopacket.meta_data.resources[:]
+
+    def remove_files(self):
+        del self.phenopacket.files[:]
+
     def clean_phenopacket(self) -> Phenopacket:
         """Clean phenopacket."""
+        self.remove_evidence_from_phenotypic_features()
+        self.check_length_phenotypic_features()
         self.remove_interpretations()
         self.remove_diseases()
         self.rename_id()
+        self.remove_metadata_resources()
+        self.remove_files()
         return self.phenopacket
