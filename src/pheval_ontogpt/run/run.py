374ba32f81155422fd5da7f556c5a0411c85069e
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-08-29 11:15:25 +0100

    Adding variable for GPT model

--- src/pheval_ontogpt/run/run.py
+++ src/pheval_ontogpt/run/run.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from pheval_ontogpt.run.run_basic_pheno_engine import run_phenopackets
 
 
-def run_basic(testdata_dir: Path, raw_results_dir: Path):
+def run_basic(testdata_dir: Path, raw_results_dir: Path, model: str):
     """Run basic pheno engine on a directory of phenopackets."""
     phenopacket_dir = testdata_dir.joinpath("phenopackets")
-    run_phenopackets(phenopacket_dir, raw_results_dir)
+    run_phenopackets(phenopacket_dir, raw_results_dir, model)
