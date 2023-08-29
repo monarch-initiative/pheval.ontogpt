d71bc051189670fac67bf51ced980cdb00cd0ad7
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-08-29 11:13:57 +0100

    Adding detail to docstrings

--- src/pheval_ontogpt/prepare/clean_phenopacket.py
+++ src/pheval_ontogpt/prepare/clean_phenopacket.py
@@ -1,9 +1,12 @@
 from phenopackets import Phenopacket
 from pheval.utils.phenopacket_utils import PhenopacketUtil
-import random
 
 
 class PhenopacketCleaner:
+    """
+    Class for 'cleaning' a phenopacket from any disease-identifying elements including:
+    disease and interpretation objects,
+    """
     def __init__(self, phenopacket: Phenopacket):
         self.phenopacket = phenopacket
 
@@ -20,28 +23,29 @@ class PhenopacketCleaner:
         self.phenopacket.id = "patient1"
 
     def remove_evidence_from_phenotypic_features(self):
+        """Remove the evidence for an assertion of the observed phenotype."""
         for phenotypic_feature in PhenopacketUtil(self.phenopacket).phenotypic_features():
             del phenotypic_feature.evidence[:]
 
-    def check_length_phenotypic_features(self):
-        if len(PhenopacketUtil(self.phenopacket).phenotypic_features()) > 50:
-            selected = random.sample(PhenopacketUtil(self.phenopacket).phenotypic_features(), 50)
-            del self.phenopacket.phenotypic_features[:]
-            self.phenopacket.phenotypic_features.extend(selected)
+    def clear_meta_data(self):
+        """Clear the meta_data element from phenopacket."""
+        self.phenopacket.meta_data.Clear()
 
-    def remove_metadata_resources(self):
-        del self.phenopacket.meta_data.resources[:]
+    def clear_taxonomy(self):
+        """Clear the taxonomy element from phenopacket."""
+        self.phenopacket.subject.taxonomy.Clear()
 
     def remove_files(self):
+        """Remove files object from phenopacket."""
         del self.phenopacket.files[:]
 
     def clean_phenopacket(self) -> Phenopacket:
-        """Clean phenopacket."""
+        """Clean a phenopacket from extra elements not required for disease prediction."""
         self.remove_evidence_from_phenotypic_features()
-        self.check_length_phenotypic_features()
         self.remove_interpretations()
         self.remove_diseases()
         self.rename_id()
-        self.remove_metadata_resources()
+        self.clear_taxonomy()
+        self.clear_meta_data()
         self.remove_files()
         return self.phenopacket
