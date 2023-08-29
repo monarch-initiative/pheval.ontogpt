ecca9a072606ba92a87f1b3613262644b4fa23dc
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-08-29 11:15:50 +0100

    parsing tool specific config

--- src/pheval_ontogpt/runner.py
+++ src/pheval_ontogpt/runner.py
@@ -6,6 +6,7 @@ from pheval.runners.runner import PhEvalRunner
 
 from pheval_ontogpt.post_process.post_process import post_process_results_format
 from pheval_ontogpt.run.run import run_basic
+from pheval_ontogpt.tool_specific_configuration_parser import OntoGPTToolSpecificConfigurations
 
 
 @dataclass
@@ -26,7 +27,10 @@ class OntoGPTPhEvalRunner(PhEvalRunner):
     def run(self):
         """run"""
         print("running with OntoGPT")
-        run_basic(self.testdata_dir, self.raw_results_dir)
+        tool_specific_configurations = OntoGPTToolSpecificConfigurations.parse_obj(
+            self.input_dir_config.tool_specific_configuration_options
+        )
+        run_basic(self.testdata_dir, self.raw_results_dir, tool_specific_configurations.model)
 
     def post_process(self):
         """post_process"""
