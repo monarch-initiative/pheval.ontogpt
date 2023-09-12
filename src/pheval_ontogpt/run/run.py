05f4cbc5abc823bc7d5aa3d081195ff8b8356585
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-09-12 11:43:03 +0100

    adding disease/gene analysis parameters

--- src/pheval_ontogpt/run/run.py
+++ src/pheval_ontogpt/run/run.py
@@ -3,7 +3,13 @@ from pathlib import Path
 from pheval_ontogpt.run.run_basic_pheno_engine import run_phenopackets
 
 
-def run_basic(testdata_dir: Path, raw_results_dir: Path, model: str):
+def run_basic(
+    testdata_dir: Path,
+    raw_results_dir: Path,
+    model: str,
+    gene_analysis: bool,
+    disease_analysis: bool,
+):
     """Run basic pheno engine on a directory of phenopackets."""
     phenopacket_dir = testdata_dir.joinpath("phenopackets")
-    run_phenopackets(phenopacket_dir, raw_results_dir, model)
+    run_phenopackets(phenopacket_dir, raw_results_dir, model, gene_analysis, disease_analysis)
