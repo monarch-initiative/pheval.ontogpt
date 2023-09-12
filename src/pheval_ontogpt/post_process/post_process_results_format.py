fa524d6c0e302cd78fcdfe1bfcdd765969bd647d
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-09-12 11:42:42 +0100

    adding Mondo extraction alternative and implementing separated disease/gene standardisation

--- src/pheval_ontogpt/post_process/post_process_results_format.py
+++ src/pheval_ontogpt/post_process/post_process_results_format.py
@@ -57,6 +57,55 @@ class PhEvalDiseaseResultFromOntoGPT:
         return pheval_disease_results
 
 
+class PhEvalDiseaseResultFromOntoGPTMondo:
+    def __init__(self, ontogpt_result: [dict]):
+        self.ontogpt_result = ontogpt_result
+
+    @staticmethod
+    def obtain_score(result: dict) -> float:
+        """Obtain score."""
+        return result["score"]
+
+    @staticmethod
+    def obtain_omim_disease_id(result: dict) -> str:
+        """Obtain omim disease ID."""
+        return result["omim_disease_id"]
+
+    @staticmethod
+    def obtain_disease_name(result: dict) -> str:
+        """Obtain disease name."""
+        return result["disease_name"]
+
+    @staticmethod
+    def mondo_found(mondo_ids: []) -> bool:
+        for mondo_id in mondo_ids:
+            if mondo_id.startswith("MONDO"):
+                return True
+
+    def extract_pheval_requirements(self) -> [PhEvalDiseaseResult]:
+        """Extract PhEval disease requirements."""
+        pheval_disease_results = []
+        for result in self.ontogpt_result:
+            if self.mondo_found(result["disease_ids"]):
+                for mondo_id in result["disease_ids"]:
+                    pheval_disease_results.append(
+                        PhEvalDiseaseResult(
+                            disease_name=self.obtain_disease_name(result),
+                            disease_identifier=mondo_id,
+                            score=self.obtain_score(result),
+                        )
+                    )
+            else:
+                pheval_disease_results.append(
+                    PhEvalDiseaseResult(
+                        disease_name=self.obtain_disease_name(result),
+                        disease_identifier=self.obtain_omim_disease_id(result),
+                        score=self.obtain_score(result),
+                    )
+                )
+        return pheval_disease_results
+
+
 class PhEvalGeneResultFromOntoGPT:
     def __init__(self, ontogpt_result: [dict], gene_identifier_updator: GeneIdentifierUpdater):
         self.ontogpt_result = ontogpt_result
@@ -98,33 +147,55 @@ def create_empty_results_file(output_dir: Path, result: Path, file_type: str):
 
 
 def create_standardised_results(
-    raw_results_dir: Path, output_dir: Path, sort_order: str = "descending"
+    raw_results_dir: Path,
+    output_dir: Path,
+    gene_analysis: bool,
+    disease_analysis: bool,
+    sort_order: str = "descending",
 ) -> None:
-    """Write standardised variant results from OntoGPT json output."""
+    """Write standardised PhEval results from OntoGPT json output."""
+
     gene_identifier_updator = GeneIdentifierUpdater(
         hgnc_data=create_hgnc_dict(), gene_identifier="ensembl_id"
     )
-    for result in files_with_suffix(raw_results_dir, ".json"):
+
+    def handle_empty_results(result_file: Path):
+        if disease_analysis:
+            create_empty_results_file(output_dir, result_file, "disease")
+        if gene_analysis:
+            create_empty_results_file(output_dir, result_file, "gene")
+
+    for ontogpt_result_file in files_with_suffix(raw_results_dir, ".json"):
         try:
-            ontogpt_result = read_ontogpt_result(result)
+            ontogpt_result = read_ontogpt_result(ontogpt_result_file)
+
             if len(ontogpt_result) == 0:
-                create_empty_results_file(output_dir, result, "disease")
-                create_empty_results_file(output_dir, result, "gene")
-            pheval_disease_result = PhEvalDiseaseResultFromOntoGPT(
-                ontogpt_result
-            ).extract_pheval_requirements()
-            generate_pheval_result(
-                pheval_disease_result, sort_order, output_dir, trim_ontogpt_result(result)
-            )
-            pheval_gene_result = PhEvalGeneResultFromOntoGPT(
-                ontogpt_result, gene_identifier_updator
-            ).extract_pheval_requirements()
-            generate_pheval_result(
-                pheval_gene_result, sort_order, output_dir, trim_ontogpt_result(result)
-            )
+                handle_empty_results(ontogpt_result_file)
+
+            if disease_analysis:
+                pheval_disease_result = PhEvalDiseaseResultFromOntoGPT(
+                    ontogpt_result
+                ).extract_pheval_requirements()
+                generate_pheval_result(
+                    pheval_disease_result,
+                    sort_order,
+                    output_dir,
+                    trim_ontogpt_result(ontogpt_result_file),
+                )
+
+            if gene_analysis:
+                pheval_gene_result = PhEvalGeneResultFromOntoGPT(
+                    ontogpt_result, gene_identifier_updator
+                ).extract_pheval_requirements()
+                generate_pheval_result(
+                    pheval_gene_result,
+                    sort_order,
+                    output_dir,
+                    trim_ontogpt_result(ontogpt_result_file),
+                )
+
         except KeyError:
-            create_empty_results_file(output_dir, result, "disease")
-            create_empty_results_file(output_dir, result, "gene")
+            handle_empty_results(ontogpt_result_file)
 
 
 @click.command("standardise")
@@ -144,7 +215,27 @@ def create_standardised_results(
     help="Full path to Ontogpt results directory to be standardised.",
     type=Path,
 )
-def create_standardised_results_command(raw_results_dir: Path, output_dir: Path):
-    output_dir.joinpath("pheval_disease_results").mkdir(exist_ok=True)
-    output_dir.joinpath("pheval_gene_results").mkdir(exist_ok=True)
-    create_standardised_results(raw_results_dir, output_dir)
+@click.option(
+    "--gene-analysis/--no-gene-analysis",
+    default=False,
+    required=False,
+    type=bool,
+    show_default=True,
+    help="Specify analysis for gene prioritisation",
+)
+@click.option(
+    "--disease-analysis/--no-disease-analysis",
+    default=False,
+    required=False,
+    type=bool,
+    show_default=True,
+    help="Specify analysis for disease prioritisation",
+)
+def create_standardised_results_command(
+    raw_results_dir: Path, output_dir: Path, gene_analysis: bool, disease_analysis: bool
+):
+    if disease_analysis:
+        output_dir.joinpath("pheval_disease_results").mkdir(exist_ok=True)
+    if gene_analysis:
+        output_dir.joinpath("pheval_gene_results").mkdir(exist_ok=True)
+    create_standardised_results(raw_results_dir, output_dir, gene_analysis, disease_analysis)
