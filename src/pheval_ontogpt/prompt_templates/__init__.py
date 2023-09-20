309c815b1c138a13dfb012224cd406555932f5fe
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-09-20 17:59:25 +0100

    refactoring joint prompt template name

--- src/pheval_ontogpt/prompt_templates/__init__.py
+++ src/pheval_ontogpt/prompt_templates/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 PHENOPACKET_PROMPT_DIR_PATH = Path(__file__).parent
-DEFAULT_PHENOPACKET_PROMPT = PHENOPACKET_PROMPT_DIR_PATH / "template.jinja2"
+JOINT_PHENOPACKET_PROMPT = PHENOPACKET_PROMPT_DIR_PATH / "joint_request_template.jinja2"
 GENE_PHENOPACKET_PROMPT = PHENOPACKET_PROMPT_DIR_PATH / "gene_request_template.jinja2"
 DISEASE_PHENOPACKET_PROMPT = PHENOPACKET_PROMPT_DIR_PATH / "disease_request_template.jinja2"
