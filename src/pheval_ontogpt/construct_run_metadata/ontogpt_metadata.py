dc8d612eea6dcc8626a1b742353f5f2dd637e751
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-09-21 23:01:56 +0100

    tox lint

--- src/pheval_ontogpt/construct_run_metadata/ontogpt_metadata.py
+++ src/pheval_ontogpt/construct_run_metadata/ontogpt_metadata.py
@@ -1,9 +1,10 @@
-from datetime import datetime
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass
 class OntoGPTMetaData:
     """Tool specific metadata."""
+
     api_call_date: datetime
     gpt_model: str
