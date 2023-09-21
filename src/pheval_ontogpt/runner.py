a7da9821f212e7076446c15db56c6319e920ba25
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-09-21 23:00:19 +0100

    adding method to add tool specific metadata to results.yml

--- src/pheval_ontogpt/runner.py
+++ src/pheval_ontogpt/runner.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from pheval.runners.runner import PhEvalRunner
 
+from pheval_ontogpt.construct_run_metadata.construct_metadata import construct_run_metadata
 from pheval_ontogpt.post_process.post_process import post_process_results_format
 from pheval_ontogpt.run.run import run_basic
 from pheval_ontogpt.tool_specific_configuration_parser import OntoGPTToolSpecificConfigurations
@@ -47,3 +48,9 @@ class OntoGPTPhEvalRunner(PhEvalRunner):
             self.input_dir_config.gene_analysis,
             self.input_dir_config.disease_analysis,
         )
+
+    def construct_meta_data(self):
+        tool_specific_configurations = OntoGPTToolSpecificConfigurations.parse_obj(
+            self.input_dir_config.tool_specific_configuration_options
+        )
+        return construct_run_metadata(self.meta_data, tool_specific_configurations.model)
