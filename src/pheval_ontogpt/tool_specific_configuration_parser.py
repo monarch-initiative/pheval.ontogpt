2d15ee4ecdf114d1d813afbc47fd2a5d8a3a8f1f
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-12-04 16:13:46 +0000

    adding template and constrained list path to config

--- src/pheval_ontogpt/tool_specific_configuration_parser.py
+++ src/pheval_ontogpt/tool_specific_configuration_parser.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+
 from pydantic import BaseModel, Field
 
 
 class OntoGPTToolSpecificConfigurations(BaseModel):
     model: str = Field(...)
+    template: Path = Field(...)
+    constrained_list_path: Path = Field(None)
