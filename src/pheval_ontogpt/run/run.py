feb7ae94b879ba36750efb65f7a25bd7fff336f4
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-12-04 16:15:05 +0000

    adding template and constrained list path

--- src/pheval_ontogpt/run/run.py
+++ src/pheval_ontogpt/run/run.py
@@ -4,12 +4,8 @@ from pheval_ontogpt.run.run_basic_pheno_engine import run_phenopackets
 
 
 def run_basic(
-    testdata_dir: Path,
-    raw_results_dir: Path,
-    model: str,
-    gene_analysis: bool,
-    disease_analysis: bool,
+    testdata_dir: Path, raw_results_dir: Path, model: str, prompt: Path, constrained_list_path: Path
 ):
     """Run basic pheno engine on a directory of phenopackets."""
     phenopacket_dir = testdata_dir.joinpath("phenopackets")
-    run_phenopackets(phenopacket_dir, raw_results_dir, model, gene_analysis, disease_analysis)
+    run_phenopackets(phenopacket_dir, raw_results_dir, model, prompt, constrained_list_path)
--- src/pheval_ontogpt/run/run_basic_pheno_engine.py
+++ src/pheval_ontogpt/run/run_basic_pheno_engine.py
@@ -6,24 +6,29 @@ from pheval.utils.file_utils import all_files
 from pheval.utils.phenopacket_utils import phenopacket_reader
 
 from pheval_ontogpt.prepare.clean_phenopacket import PhenopacketCleaner
-from pheval_ontogpt.prompt_templates import (
-    DISEASE_PHENOPACKET_PROMPT,
-    GENE_PHENOPACKET_PROMPT,
-    JOINT_PHENOPACKET_PROMPT,
-)
 from pheval_ontogpt.run.basic_pheno_engine import PhenoEngine
 
 
 def run_phenopacket(
-    pheno_engine: PhenoEngine, phenopacket: Phenopacket, gene_analysis: bool, disease_analysis: bool
+    pheno_engine: PhenoEngine,
+    phenopacket: Phenopacket,
+    prompt_template: Path,
+    constrained_list_path: Path = None,
 ):
     """Run pheno engine on a single phenopacket."""
-    if gene_analysis and disease_analysis:
-        return pheno_engine.predict(phenopacket, JOINT_PHENOPACKET_PROMPT)
-    elif gene_analysis:
-        return pheno_engine.predict(phenopacket, GENE_PHENOPACKET_PROMPT)
-    elif disease_analysis:
-        return pheno_engine.predict(phenopacket, DISEASE_PHENOPACKET_PROMPT)
+    if constrained_list_path is not None:
+        with open(constrained_list_path, "r") as f:
+            constrained_list = f.readlines()
+        f.close()
+    else:
+        constrained_list = None
+    return pheno_engine.predict(phenopacket, prompt_template, constrained_list)
+    # if gene_analysis and disease_analysis:
+    #     return pheno_engine.predict(phenopacket, JOINT_PHENOPACKET_PROMPT)
+    # elif gene_analysis:
+    #     return pheno_engine.predict(phenopacket, GENE_PHENOPACKET_PROMPT)
+    # elif disease_analysis:
+    #     return pheno_engine.predict(phenopacket, DISEASE_PHENOPACKET_PROMPT)
 
 
 def write_json_result(
@@ -40,13 +45,13 @@ def run_phenopackets(
     phenopacket_dir: Path,
     raw_results_dir: Path,
     model: str,
-    gene_analysis: bool,
-    disease_analysis: bool,
+    prompt: Path,
+    constrained_list_path: Path = None,
 ) -> None:
     """Run a directory of phenopackets on the basic PhenoEngine."""
     pheno_engine = PhenoEngine(model=model)
     for phenopacket_path in all_files(phenopacket_dir):
         phenopacket = phenopacket_reader(phenopacket_path)
         clean_phenopacket = PhenopacketCleaner(phenopacket).clean_phenopacket()
-        result = run_phenopacket(pheno_engine, clean_phenopacket, gene_analysis, disease_analysis)
+        result = run_phenopacket(pheno_engine, clean_phenopacket, prompt, constrained_list_path)
         write_json_result(result, raw_results_dir, phenopacket_path)
